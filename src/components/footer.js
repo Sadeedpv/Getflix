9bc795d55125c3551779ebc3b46b0f96dfc343f6
unknown <muhammedsadeed70@gmail.com>
2022-05-21 19:56:23 +0400

    footer redesigned

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,8 @@ function Footer() {
   return (
     <div className='footer'>
         <footer>
-            <p>Made with &nbsp;<FaHeart style={{color: "red"}} /> &nbsp;by <strong>Sadeed pv</strong></p>
-            <p>Follow me on &nbsp;<a href='https://github.com/Sadeedpv' className='href'><FaGithub /></a></p>
+            <p className="nowrap">Made with &nbsp;<FaHeart style={{color: "red"}} /> &nbsp;by <strong>Sadeed pv</strong></p>
+            <p className="nowrap">Follow me on &nbsp;<a href='https://github.com/Sadeedpv' className='href'><FaGithub /></a></p>
         </footer>
     </div>
   )
